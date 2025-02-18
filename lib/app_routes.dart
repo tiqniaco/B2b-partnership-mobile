f01701fd55b76f90a55ec4943136cbc16141c409
ahlam <ahlammagdy333@gmail.com>
2025-02-18 17:32:50 +0200

    routes

--- lib/app_routes.dart
+++ lib/app_routes.dart
@@ -13,6 +13,7 @@ import 'package:b2b_partenership/views/service_details_view.dart';
 import 'package:b2b_partenership/views/in_category/providers_in_categories.dart';
 import 'package:b2b_partenership/views/service_request/add_service_request.dart';
 import 'package:b2b_partenership/views/service_request/get_user_service_request.dart';
+import 'package:b2b_partenership/views/shop/shop_cart_view.dart';
 import 'package:b2b_partenership/views/shop/shop_view.dart';
 import 'package:b2b_partenership/views/splash/views/splash_view.dart';
 import 'package:get/get.dart';
