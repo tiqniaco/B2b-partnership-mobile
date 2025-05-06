a7fd0c4608ff317600d6bde6a970a9770c5d9c3c
Assem <assemabudeif@gmail.com>
2025-05-06 10:51:04 +0300

    Refactor ProviderModel to use immutable fields and implement Equatable for improved performance and comparison

--- lib/models/provider_model.dart
+++ lib/models/provider_model.dart
@@ -1,161 +1,210 @@
 import 'package:b2b_partenership/core/constants/app_constants.dart';
+import 'package:equatable/equatable.dart';
 
-class ProviderModel {
-  String? userId;
-  String? name;
-  String? email;
-  String? countryCode;
-  String? phone;
-  String? image;
-  String? providerId;
-  String? commercialRegister;
-  String? taxCard;
-  String? bio;
-  String? rating;
-  String? providerTypeId;
-  String? providerTypeNameAr;
-  String? providerTypeNameEn;
-  String? specializationId;
-  String? specializationNameAr;
-  String? specializationNameEn;
-  String? subSpecializationId;
-  String? subSpecializationNameAr;
-  String? subSpecializationNameEn;
-  String? countryId;
-  String? countryNameAr;
-  String? countryNameEn;
-  String? governmentId;
-  String? governmentNameAr;
-  String? governmentNameEn;
-  String? contactPhone;
-  String? contactEmail;
-  String? contactWhatsapp;
-  String? contactTelegram;
-  String? contactInstagram;
-  String? contactFacebook;
-  String? contactLinkedin;
-  String? contactWebsite;
-  String? createdAt;
-  String? updatedAt;
-  String? isFavorite;
+class ProviderModel extends Equatable {
+  final String userId;
+  final String name;
+  final String email;
+  final String countryCode;
+  final String phone;
+  final String image;
+  final String status;
+  final String providerId;
+  final String commercialRegister;
+  final String taxCard;
+  final String bio;
+  final String rating;
+  final String providerVerifiedCode;
+  final String providerTypeId;
+  final String providerTypeNameAr;
+  final String providerTypeNameEn;
+  final String specializationId;
+  final String specializationNameAr;
+  final String specializationNameEn;
+  final String subSpecializationId;
+  final String subSpecializationNameAr;
+  final String subSpecializationNameEn;
+  final String countryId;
+  final String countryNameAr;
+  final String countryNameEn;
+  final String governmentId;
+  final String governmentNameAr;
+  final String governmentNameEn;
+  final String contactPhone;
+  final String contactEmail;
+  final String contactWhatsapp;
+  final String contactTelegram;
+  final String contactInstagram;
+  final String contactFacebook;
+  final String contactLinkedin;
+  final String contactWebsite;
+  final String createdAt;
+  final String updatedAt;
+  final String isFavorite;
 
-  ProviderModel(
-      {this.userId,
-      this.name,
-      this.email,
-      this.countryCode,
-      this.phone,
-      this.image,
-      this.providerId,
-      this.commercialRegister,
-      this.taxCard,
-      this.bio,
-      this.rating,
-      this.providerTypeId,
-      this.providerTypeNameAr,
-      this.providerTypeNameEn,
-      this.specializationId,
-      this.specializationNameAr,
-      this.specializationNameEn,
-      this.subSpecializationId,
-      this.subSpecializationNameAr,
-      this.subSpecializationNameEn,
-      this.countryId,
-      this.countryNameAr,
-      this.countryNameEn,
-      this.governmentId,
-      this.governmentNameAr,
-      this.governmentNameEn,
-      this.contactPhone,
-      this.contactEmail,
-      this.contactWhatsapp,
-      this.contactTelegram,
-      this.contactInstagram,
-      this.contactFacebook,
-      this.contactLinkedin,
-      this.contactWebsite,
-      this.createdAt,
-      this.updatedAt,
-      this.isFavorite});
+  const ProviderModel({
+    required this.userId,
+    required this.name,
+    required this.email,
+    required this.countryCode,
+    required this.phone,
+    required this.image,
+    required this.status,
+    required this.providerId,
+    required this.commercialRegister,
+    required this.taxCard,
+    required this.bio,
+    required this.rating,
+    required this.providerVerifiedCode,
+    required this.providerTypeId,
+    required this.providerTypeNameAr,
+    required this.providerTypeNameEn,
+    required this.specializationId,
+    required this.specializationNameAr,
+    required this.specializationNameEn,
+    required this.subSpecializationId,
+    required this.subSpecializationNameAr,
+    required this.subSpecializationNameEn,
+    required this.countryId,
+    required this.countryNameAr,
+    required this.countryNameEn,
+    required this.governmentId,
+    required this.governmentNameAr,
+    required this.governmentNameEn,
+    required this.contactPhone,
+    required this.contactEmail,
+    required this.contactWhatsapp,
+    required this.contactTelegram,
+    required this.contactInstagram,
+    required this.contactFacebook,
+    required this.contactLinkedin,
+    required this.contactWebsite,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.isFavorite,
+  });
 
-  ProviderModel.fromJson(Map<String, dynamic> json) {
-    userId = json['user_id'] ?? "";
-    name = json['name'] ?? "";
-    email = json['email'] ?? "";
-    countryCode = json['country_code'] ?? "";
-    phone = json['phone'] ?? "";
-    image = kBaseImageUrl + json['image'];
-    providerId = json['provider_id'] ?? "";
-    commercialRegister = kBaseImageUrl + json['commercial_register'];
-    taxCard = kBaseImageUrl + json['tax_card'];
-    bio = json['bio'] ?? "";
-    rating = json['rating'] ?? "";
-    providerTypeId = json['provider_type_id'] ?? "";
-    providerTypeNameAr = json['provider_type_name_ar'];
-    providerTypeNameEn = json['provider_type_name_en'] ?? "";
-    specializationId = json['specialization_id'] ?? "";
-    specializationNameAr = json['specialization_name_ar'] ?? "";
-    specializationNameEn = json['specialization_name_en'] ?? "";
-    subSpecializationId = json['sub_specialization_id'] ?? "";
-    subSpecializationNameAr = json['sub_specialization_name_ar'] ?? "";
-    subSpecializationNameEn = json['sub_specialization_name_en'] ?? "";
-    countryId = json['country_id'] ?? "";
-    countryNameAr = json['country_name_ar'] ?? "";
-    countryNameEn = json['country_name_en'] ?? "";
-    governmentId = json['government_id'] ?? "";
-    governmentNameAr = json['government_name_ar'] ?? "";
-    governmentNameEn = json['government_name_en'] ?? "";
-    contactPhone = json['contact_phone'] ?? "";
-    contactEmail = json['contact_email'] ?? "";
-    contactWhatsapp = json['contact_whatsapp'] ?? "";
-    contactTelegram = json['contact_telegram'] ?? "";
-    contactInstagram = json['contact_instagram'] ?? "";
-    contactFacebook = json['contact_facebook'] ?? "";
-    contactLinkedin = json['contact_linkedin'] ?? "";
-    contactWebsite = json['contact_website'] ?? "";
-    createdAt = json['created_at'] ?? "";
-    updatedAt = json['updated_at'] ?? "";
-    isFavorite = json['is_favorite'] ?? "0";
-  }
+  factory ProviderModel.fromJson(Map<String, dynamic> json) => ProviderModel(
+        userId: json["user_id"] ?? "",
+        name: json["name"] ?? "",
+        email: json["email"] ?? "",
+        countryCode: json["country_code"] ?? "",
+        phone: json["phone"] ?? "",
+        image: kBaseImageUrl + json["image"],
+        status: json["status"] ?? "",
+        providerId: json["provider_id"] ?? "",
+        commercialRegister: json["commercial_register"] ?? "",
+        taxCard: json["tax_card"] ?? "",
+        bio: json["bio"] ?? "",
+        rating: json["rating"] ?? "",
+        providerVerifiedCode: json["provider_verified_code"] ?? "",
+        providerTypeId: json["provider_type_id"] ?? "",
+        providerTypeNameAr: json["provider_type_name_ar"] ?? "",
+        providerTypeNameEn: json["provider_type_name_en"] ?? "",
+        specializationId: json["specialization_id"] ?? "",
+        specializationNameAr: json["specialization_name_ar"] ?? "",
+        specializationNameEn: json["specialization_name_en"] ?? "",
+        subSpecializationId: json["sub_specialization_id"] ?? "",
+        subSpecializationNameAr: json["sub_specialization_name_ar"] ?? "",
+        subSpecializationNameEn: json["sub_specialization_name_en"] ?? "",
+        countryId: json["country_id"] ?? "",
+        countryNameAr: json["country_name_ar"] ?? "",
+        countryNameEn: json["country_name_en"] ?? "",
+        governmentId: json["government_id"] ?? "",
+        governmentNameAr: json["government_name_ar"] ?? "",
+        governmentNameEn: json["government_name_en"] ?? "",
+        contactPhone: json["contact_phone"] ?? "",
+        contactEmail: json["contact_email"] ?? "",
+        contactWhatsapp: json["contact_whatsapp"] ?? "",
+        contactTelegram: json["contact_telegram"] ?? "",
+        contactInstagram: json["contact_instagram"] ?? "",
+        contactFacebook: json["contact_facebook"] ?? "",
+        contactLinkedin: json["contact_linkedin"] ?? "",
+        contactWebsite: json["contact_website"] ?? "",
+        createdAt: json["created_at"] ?? "",
+        updatedAt: json["updated_at"] ?? "",
+        isFavorite: json["is_favorite"] ?? "",
+      );
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['user_id'] = userId;
-    data['name'] = name;
-    data['email'] = email;
-    data['country_code'] = countryCode;
-    data['phone'] = phone;
-    data['image'] = image;
-    data['provider_id'] = providerId;
-    data['commercial_register'] = commercialRegister;
-    data['tax_card'] = taxCard;
-    data['bio'] = bio;
-    data['rating'] = rating;
-    data['provider_type_name_ar'] = providerTypeNameAr;
-    data['provider_type_name_en'] = providerTypeNameEn;
-    data['specialization_id'] = specializationId;
-    data['specialization_name_ar'] = specializationNameAr;
-    data['specialization_name_en'] = specializationNameEn;
-    data['sub_specialization_id'] = subSpecializationId;
-    data['sub_specialization_name_ar'] = subSpecializationNameAr;
-    data['sub_specialization_name_en'] = subSpecializationNameEn;
-    data['country_id'] = countryId;
-    data['country_name_ar'] = countryNameAr;
-    data['country_name_en'] = countryNameEn;
-    data['government_id'] = governmentId;
-    data['government_name_ar'] = governmentNameAr;
-    data['government_name_en'] = governmentNameEn;
-    data['contact_phone'] = contactPhone;
-    data['contact_email'] = contactEmail;
-    data['contact_whatsapp'] = contactWhatsapp;
-    data['contact_telegram'] = contactTelegram;
-    data['contact_instagram'] = contactInstagram;
-    data['contact_facebook'] = contactFacebook;
-    data['contact_linkedin'] = contactLinkedin;
-    data['contact_website'] = contactWebsite;
-    data['created_at'] = createdAt;
-    data['updated_at'] = updatedAt;
-    data['is_favorite'] = isFavorite;
-    return data;
-  }
+  Map<String, dynamic> toJson() => {
+        "user_id": userId,
+        "name": name,
+        "email": email,
+        "country_code": countryCode,
+        "phone": phone,
+        "image": image,
+        "status": status,
+        "provider_id": providerId,
+        "commercial_register": commercialRegister,
+        "tax_card": taxCard,
+        "bio": bio,
+        "rating": rating,
+        "provider_verified_code": providerVerifiedCode,
+        "provider_type_id": providerTypeId,
+        "provider_type_name_ar": providerTypeNameAr,
+        "provider_type_name_en": providerTypeNameEn,
+        "specialization_id": specializationId,
+        "specialization_name_ar": specializationNameAr,
+        "specialization_name_en": specializationNameEn,
+        "sub_specialization_id": subSpecializationId,
+        "sub_specialization_name_ar": subSpecializationNameAr,
+        "sub_specialization_name_en": subSpecializationNameEn,
+        "country_id": countryId,
+        "country_name_ar": countryNameAr,
+        "country_name_en": countryNameEn,
+        "government_id": governmentId,
+        "government_name_ar": governmentNameAr,
+        "government_name_en": governmentNameEn,
+        "contact_phone": contactPhone,
+        "contact_email": contactEmail,
+        "contact_whatsapp": contactWhatsapp,
+        "contact_telegram": contactTelegram,
+        "contact_instagram": contactInstagram,
+        "contact_facebook": contactFacebook,
+        "contact_linkedin": contactLinkedin,
+        "contact_website": contactWebsite,
+        "created_at": createdAt,
+        "updated_at": updatedAt,
+        "is_favorite": isFavorite,
+      };
+
+  @override
+  List<Object?> get props => [
+        userId,
+        name,
+        email,
+        countryCode,
+        phone,
+        image,
+        status,
+        providerId,
+        commercialRegister,
+        taxCard,
+        bio,
+        rating,
+        providerVerifiedCode,
+        providerTypeId,
+        providerTypeNameAr,
+        providerTypeNameEn,
+        specializationId,
+        specializationNameAr,
+        specializationNameEn,
+        subSpecializationId,
+        subSpecializationNameAr,
+        subSpecializationNameEn,
+        countryId,
+        countryNameAr,
+        countryNameEn,
+        governmentId,
+        governmentNameAr,
+        governmentNameEn,
+        contactPhone,
+        contactEmail,
+        contactWhatsapp,
+        contactTelegram,
+        contactInstagram,
+        contactFacebook,
+        contactLinkedin,
+        contactWebsite,
+      ];
 }
